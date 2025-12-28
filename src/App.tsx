8b190dcca79c15d49cda0cf8621079c30f7340d0
kenny <khaneesh50@gmail.com>
2025-12-28 12:29:56 +0530

    add: added theming

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { ThemeProvider } from "@/components/theme-provider";
+
 export function App() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="font-medium">Hello World</div>
-    </div>
-  )
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="font-medium">Hello World</div>
+      </div>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
