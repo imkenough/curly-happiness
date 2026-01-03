04e9e6b19a89a6f29638bfd4d08393ee54124de1
kenny <khaneesh50@gmail.com>
2026-01-03 10:34:10 +0530

    fix: fixed dashboard import

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { ThemeProvider } from "@/components/theme-provider";
+import Page from "./app/dashboard/page";
 
 export function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="flex items-center justify-center min-h-screen">
+      {/* <div className="flex items-center justify-center min-h-screen">
         <div className="font-medium">Hello World</div>
-      </div>
+      </div> */}
+      <Page></Page>
     </ThemeProvider>
   );
 }
